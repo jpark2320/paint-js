a523248b0ca47510b1dd55cfb91982c1bbc1a46e
jpark2320 <jpark2320@gmail.com>
2021-02-04 00:10:10 -0500

    Set up context & implement drawing

--- app.js
+++ app.js
@@ -1,27 +1,40 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let isPainting = false;
+
+function startPainting() {
+  isPainting = true;
+}
+
+function stopPainting() {
+  isPainting = false;
+}
 
 function onMouseMove(e) {
   const x = e.offsetX;
   const y = e.offsetY;
-}
 
-function onMouseDown(e) {
-  isPainting = true;
-}
-
-function onMouseUp(e) {
-  isPainting = false;
-}
-
-function onMouseLeave(e) {
-  isPainting = false;
+  if (!isPainting) {
+    // move path starting point
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    // draw a line from x, y to the next x, y
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mouseleave', onMouseLeave);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mouseleave', stopPainting);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
 }
